7d56e75d16f9c6155d1ca9c1a7c6e871ef09d854
Dmitrii Parfenov <parfenovds@protonmail.com>
2023-07-05 10:52:27 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.time.LocalDate;
+import org.example.converter.BirthdayConverter;
+import org.example.entity.Birthday;
+import org.example.entity.Test;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+  public static void main(String[] args) {
+    Configuration configuration = new Configuration();
+    configuration.configure();
+    configuration.addAnnotatedClass(Test.class);
+    configuration.addAttributeConverter(new BirthdayConverter(), true);
+    try(SessionFactory sessionFactory = configuration.buildSessionFactory();
+    Session session = sessionFactory.openSession()) {
+      session.beginTransaction();
+      Test test = Test.builder().text("Hello?").birthday(new Birthday(LocalDate.of(1991, 4, 15))).build();
+      session.persist(test);
+      Test test1 = session.get(Test.class, 1);
+      System.out.println(test1);
+      session.getTransaction().commit();
+    }
+  }
+}
---
+++ src/main/resources/find.sql
@@ -0,0 +1,6 @@
+DROP TABLE test;
+CREATE TABLE test (
+  id BIGSERIAL,
+  text TEXT,
+  birth_date DATE
+);
